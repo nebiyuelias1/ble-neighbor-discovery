4dc358eab40bdf40039b016626241db0a23499b8
nebiyuelias1 <nebiyuelias1@gmail.com>
2024-04-17 10:53:10 +0800

    Fix bug in ble_simulation.py

--- ble_simulation.py
+++ ble_simulation.py
@@ -44,7 +44,17 @@ class ScannerDevice:
                     break
                 
 class Simulation:
+    """
+    A class to represent a simulation of neighbor discovery of BLE devices.
+    """
     def __init__(self, rate, beacon_duration, beacon_period) -> None:
+        """This constructor initializes the simulation parameters.
+
+        Args:
+            rate (float): the rate of scanning events
+            beacon_duration (float): The duration of the beacon event
+            beacon_period (float): The time period between two beacon events
+        """
         self.rate = rate
         self.beacon_duration = beacon_duration
         self.beacon_period = beacon_period
@@ -54,32 +64,33 @@ class Simulation:
         y_k = 0
         # The index of the beacon event
         n_i = 1
+        
         while True:
             # The time since the last scanning event
+            # Start by drawing a random number from the exponential distribution
+            # Then keep adding this to y_k until it exceeds the beacon event bounds.
             x_i = np.random.exponential(scale=1/self.rate)
             y_k += x_i
             
             # The left and right bounds of the beacon event
-            a = n_i * self.beacon_period * n_i - self.beacon_duration / 2
-            b = n_i * self.beacon_period * n_i + self.beacon_duration / 2
+            a = n_i * self.beacon_period - self.beacon_duration / 2
+            b = n_i * self.beacon_period + self.beacon_duration / 2
             
+            # If the scanning event happens before the beacon event
+            # then we need to keep generating beacon events.
+            while y_k > b:
+                n_i += 1
+                a = n_i * self.beacon_period - self.beacon_duration / 2
+                b = n_i * self.beacon_period + self.beacon_duration / 2
+
             # Discovery has happened
             if a <= y_k <= b:
                 return y_k
-            
-            while y_k > b:
-                n_i += 1
-                a = n_i * self.beacon_period * n_i - self.beacon_duration / 2
-                b = n_i * self.beacon_period * n_i + self.beacon_duration / 2
-            
-                
                 
 def run_simulation(period, beacon_duration, rate):
     latency_results = []
 
     for _ in range(num_simulations):
-        beacon_events = []
-
         simulation = Simulation(rate, beacon_duration, period)
         latency_results.append(simulation.run())
 
