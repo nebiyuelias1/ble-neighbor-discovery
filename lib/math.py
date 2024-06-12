902df104c8de2740503ebbef9276169e909f1344
Nebiyu Elias <nebiyuelias1@gmail.com>
2024-06-12 21:34:00 +0800

    chore: Improve Erlang PDF computation using logarithms

--- lib/math.py
+++ lib/math.py
@@ -1,8 +1,23 @@
 import numpy as np
+from scipy.special import gammaln
+from scipy.integrate import quad
 
-# Define the PDF function
-def erlang_pdf(x, k, lambda_):
-    return lambda_**k * x**(k-1) * np.exp(-lambda_ * x) / np.math.factorial(k-1)
+def erlang_pdf(x, k, lambd):
+    """
+    Compute the Erlang PDF at x using logarithms to avoid overflow.
+    
+    Parameters:
+    x (float): The point at which to evaluate the PDF.
+    k (int): The shape parameter of the Erlang distribution.
+    lambd (float): The rate parameter of the Erlang distribution.
+    
+    Returns:
+    float: The value of the Erlang PDF at x.
+    """
+    if x < 0:
+        return 0
+    log_pdf = (k * np.log(lambd) + (k - 1) * np.log(x) - lambd * x - gammaln(k))
+    return np.exp(log_pdf)
 
 def poisson_pmf(k, nL, lambda_):
     return (nL*lambda_)**k * np.exp(-nL*lambda_) / np.math.factorial(k)
