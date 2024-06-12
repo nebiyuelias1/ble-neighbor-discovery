ee57c9f2d4eb18655c085b8fddbf9df357937561
Nebiyu Elias <nebiyuelias1@gmail.com>
2024-06-12 21:34:14 +0800

    chore: Add analytical latency calculation function

--- lib/math.py
+++ lib/math.py
@@ -21,3 +21,31 @@ def erlang_pdf(x, k, lambd):
 
 def poisson_pmf(k, nL, lambda_):
     return (nL*lambda_)**k * np.exp(-nL*lambda_) / np.math.factorial(k)
+
+
+def analytical_latency_result(n_limit, k_limit, interval, omega, rate):
+    P_n = []
+    
+    for n in range(1, n_limit + 1):
+        lower_bound = n * interval - omega/2
+        upper_bound = n * interval + omega/2
+
+        sum_k = 0
+        for k in range(1, k_limit + 1):
+            erlang_pdf_res, error = quad(erlang_pdf, lower_bound, upper_bound, args=(k, rate))
+            sum_k += erlang_pdf_res
+        P_n.append(sum_k)
+
+    latency = 0
+    bernouli_probabilities = []
+    for n in range(n_limit):
+        time_duration = (n + 1) * interval
+        probability_of_no_match = np.prod([1 - P_n[i] for i in range(n)])
+        bernouli_prob =  P_n[n] * probability_of_no_match
+        bernouli_probabilities.append(bernouli_prob)
+        latency += time_duration * bernouli_prob
+    print(f'latency: {latency}')
+    print(f'sum (P_n): {sum(bernouli_probabilities)}')
+            
+    return latency
+
